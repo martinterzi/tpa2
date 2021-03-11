f2665ee5d31e407b035776f3b3b6669e834b3196
unknown <martin.terzi@acquappessa.com.ar>
2021-03-11 12:16:58 -0200

    cambio5

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function mostrarAlerta(mensaje) {
 function buscarImagenes() {
     const terminoBusqueda = document.querySelector('#termino').value;
 
-    const key = 'Uva2haK5rm1yEoCYsd2zUqiLjxonxwd9M';
+    const key = 'va2haK5rm1yEoCYsd2zUqiLjxonxwd9M';
     const url = (`https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=12&q=${terminoBusqueda}`);
     
     fetch(url)
